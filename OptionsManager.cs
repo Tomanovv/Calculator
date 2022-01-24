8533da8a05b63e814d99ec9cadd998b666576798
unknown <georgi0250@abv.bg>
2022-01-24 22:13:43 +0200

    commit1

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -5,14 +5,18 @@ namespace GitExercise
     public static class OptionsManager
     {
         public static string[] OptionsList = {
-            "a - Add"
+            "a - Add","s-Substract"
         };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
+        public static void Substract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
 
-       
+
     }
 }
